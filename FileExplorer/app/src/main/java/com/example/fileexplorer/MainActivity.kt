a5455a04d5ced722caf2c140b12860bc385125b2
Aierse <68111814+Aierse@users.noreply.github.com>
2022-01-18 17:51:56 +0900

    파일탐색기 UI구현(이벤트 미구현)

--- FileExplorer/app/src/main/java/com/example/fileexplorer/MainActivity.kt
+++ FileExplorer/app/src/main/java/com/example/fileexplorer/MainActivity.kt
@@ -1,17 +1,66 @@
 package com.example.fileexplorer
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.fileexplorer.adapters.FileAdapter
 import com.example.fileexplorer.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    private val READ_STORAGE_PERMISSION = 100
     private lateinit var binding: ActivityMainBinding
+    private val EXTERNAL_STORAGE_PATH = Environment.getExternalStorageDirectory().toString()
+    lateinit var fileAdapter: FileAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            afterGranted(EXTERNAL_STORAGE_PATH)
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), READ_STORAGE_PERMISSION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == READ_STORAGE_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                afterGranted(EXTERNAL_STORAGE_PATH)
+            }
+            else {
+                if(!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))  {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + BuildConfig.APPLICATION_ID))
+                    startActivity(intent)
+
+                    Toast.makeText(applicationContext, "권한 설정 후 다시 실행 해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(this, "권한 거부되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun afterGranted(path: String) {
+        val files = File(path).listFiles().toCollection(ArrayList<File>())
+
+        fileAdapter = FileAdapter(this, R.layout.file_list_item, files)
+
+        binding.fileListView.adapter = fileAdapter
 
     }
 }
---
+++ FileExplorer/app/src/main/java/com/example/fileexplorer/adapters/FileAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.fileexplorer.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.fileexplorer.R
+import java.io.File
+
+class FileAdapter(
+    private val mContext: Context,
+    private val resId: Int,
+    private val mList: List<File>) : ArrayAdapter<File>(mContext, resId, mList) {
+    private val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.file_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val file = mList[position]
+        val fileName = row.findViewById<TextView>(R.id.fileName)
+        val fileSize = row.findViewById<TextView>(R.id.fileSize)
+        val modifiedDate = row.findViewById<TextView>(R.id.modifiedDate)
+
+        if(file.isDirectory) {
+            fileName.text = file.name
+            fileSize.text = ""
+            modifiedDate.text = ""
+        }
+        else {
+            fileName.text = file.name
+            fileSize.text = file.length().toString()
+            modifiedDate.text = file.lastModified().toString()
+        }
+
+        return tempRow!!
+    }
+}
