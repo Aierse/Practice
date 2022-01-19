9ccd78b3bda83027f3b96a2995389a83230a07d3
Aierse <68111814+Aierse@users.noreply.github.com>
2022-01-19 15:23:09 +0900

    구현 - 뷰바인딩 적용

--- FileExplorer/app/src/main/java/com/example/fileexplorer/TextViewer.kt
+++ FileExplorer/app/src/main/java/com/example/fileexplorer/TextViewer.kt
@@ -2,19 +2,19 @@ package com.example.fileexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fileexplorer.databinding.ActivityMainBinding
+import com.example.fileexplorer.databinding.ActivityTextViewerBinding
 import java.io.File
 
 class TextViewer : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityTextViewerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityTextViewerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val file = intent.getStringExtra(("file"))
+        val file = intent.getSerializableExtra("file") as File
 
-        binding.fileContent.text = file
+        binding.fileContent.text = file.name
     }
 }
