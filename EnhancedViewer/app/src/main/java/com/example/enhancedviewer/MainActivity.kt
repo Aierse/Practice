0d0246fac9ecc1eae2cc9eee717639560bae9057
Aierse <dbsgur2008@gmail.com>
2022-01-20 22:31:25 +0900

    구현 - onActivityResult

--- EnhancedViewer/app/src/main/java/com/example/enhancedviewer/MainActivity.kt
+++ EnhancedViewer/app/src/main/java/com/example/enhancedviewer/MainActivity.kt
@@ -2,14 +2,15 @@ package com.example.enhancedviewer
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.enhancedviewer.databinding.ActivityMainBinding
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.lang.StringBuilder
-import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_OPEN_FILE = 40
@@ -28,9 +29,25 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, REQUEST_OPEN_FILE)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_OPEN_FILE -> {
+                    data?.let { it ->
+                        var currentUri = it.data
+                        currentUri?.let {
+                            val content = readFile(it)
+                            binding.textView.text = content
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    private fun readFile(uri: URI): String {
+    private fun readFile(uri: Uri): String {
         val stringBuilder = StringBuilder()
 
         try {
@@ -47,6 +64,8 @@ class MainActivity : AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+
+        Toast.makeText(this, stringBuilder.toString() + "AAAA", Toast.LENGTH_SHORT)
 
         return stringBuilder.toString()
     }
