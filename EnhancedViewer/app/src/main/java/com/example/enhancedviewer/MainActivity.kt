22c2361e5a5829d29eeb8d056ee960ffef4d7400
Aierse <dbsgur2008@gmail.com>
2022-01-20 22:04:33 +0900

    구현 - 파일탐색

--- EnhancedViewer/app/src/main/java/com/example/enhancedviewer/MainActivity.kt
+++ EnhancedViewer/app/src/main/java/com/example/enhancedviewer/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.enhancedviewer
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.enhancedviewer.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_OPEN_FILE = 40
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+        }
+
+        startActivityForResult(intent, REQUEST_OPEN_FILE)
     }
+
+    
 }
