7af9ecafd2fd35970d98106cfc749d94e4fadc29
Aierse <68111814+Aierse@users.noreply.github.com>
2022-01-27 18:09:34 +0900

    구현 - 필터 companian object

--- EnhancedViewer/app/src/main/java/com/example/enhancedviewer/Filter.kt
+++ EnhancedViewer/app/src/main/java/com/example/enhancedviewer/Filter.kt
@@ -1,5 +1,18 @@
 package com.example.enhancedviewer
 
 class Filter {
+    private var bracketOpen: Boolean = false
+    private var quataOpen: Boolean = false
 
+    companion object {
+        private val bracket: CharArray = charArrayOf('{', '[', '(')
+        private val termination: CharArray = charArrayOf('.', ',', '!', '?')
+
+        fun isBracket(value: Char): Boolean = bracket.contains(value)
+        fun isTermination(value: Char): Boolean = termination.contains(value)
+    }
+
+    fun arragement(value: String) {
+
+    }
 }
