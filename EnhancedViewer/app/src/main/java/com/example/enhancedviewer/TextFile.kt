4b4a2331671481f60f8cd91432a13bd6edd36d5c
Aierse <68111814+Aierse@users.noreply.github.com>
2022-02-04 13:41:11 +0900

    추가 - TextFile 클래스

---
+++ EnhancedViewer/app/src/main/java/com/example/enhancedviewer/TextFile.kt
@@ -0,0 +1,16 @@
+package com.example.enhancedviewer
+
+import java.io.File
+
+class TextFile {
+    companion object {
+        val fileList: ArrayList<TextFile> = arrayListOf<TextFile>()
+
+        fun initialize() {
+        }
+    }
+
+    constructor(name: String) {
+
+    }
+}
